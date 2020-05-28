a97ee2b75f215833a7feac348bbaea81f08ead4b
daoqing99 <daoqing99@163.com>
2020-05-28 18:27:37 +0800

    图片的上传及删除

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,11 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
+		<image class="logo" :src="imgUrl"></image>
 		<view class="text-area">
-			<text class="title">{{title}}</text>
+			<!-- <text class="title">{{title}}</text> -->
 			<button type="default" @click="open()">执行云函数</button>
+			<button type="default" @click="upLoad()">上传图片</button>
+			<button type="default" @click="delImg()">删除图片</button>
 		</view>
 	</view>
 </template>
@@ -12,13 +14,48 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: 'Hello',
+				imgUrl:'/static/logo.png'
 			}
 		},
 		onLoad() {
 		
 		},
 		methods: {
+			delImg(){
+				uniCloud.deleteFile({
+					fileList:['https://vkceyugu.cdn.bspapp.com/VKCEYUGU-uninewstest/36d59bd0-a0cd-11ea-a30b-e311646dfaf2.jpeg'],
+					success: (res) => {
+						console.log(res)
+					},
+					fail(err){
+						console.log(err)
+					}
+				})
+			},
+			upLoad(){
+				uni.chooseImage({
+					count:1,
+					success:(res) => {
+						const tempFile = res.tempFilePaths[0]
+						console.log(tempFile)
+						uniCloud.uploadFile({
+							filePath:tempFile,
+							success: (res) => {
+								console.log(res)
+								this.imgUrl = res.fileID
+								// this.imgUrl = res.filePath
+							},
+							fail(err){
+								console.log(err)
+							}
+						})
+					},
+					fail(err) {
+						console.log(err)
+					}
+				})
+			},
 			open(){
 				uniCloud.callFunction({
 					name:'get_list',
